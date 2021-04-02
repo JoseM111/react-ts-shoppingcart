ec78d4bb5678cdf1a3f33cfe85537ab72684adfa
Jose Martinez <alias_one11@outlook.com>
2021-04-03 01:17:28 +0200

    New commit...

--- src/components/cart-items/CartItem.tsx
+++ src/components/cart-items/CartItem.tsx
@@ -2,6 +2,7 @@
 import { FC } from 'react'
 import { CartItemProps } from 'App'
 import { CartItemWrapper } from './CartItem.styles'
+import Button from '@material-ui/core/Button'
 /** #™━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ */
 
 //: - ©| PROPS |
@@ -19,15 +20,65 @@ export type ItemProps = {
 const CartItem: FC<ItemProps> = ({ item, addToCart, removeFromCart }) => (
     // #™━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
     <CartItemWrapper>
-        <div >
+        <div>
 
             {/*━━━━━(| Title |)━━━━━*/ }
             <h3>{ item.title }</h3>
-            
+
+            {/*━━━━━(| Cart Information |)━━━━━*/ }
+            <div className="information">
+                {/**━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━*/ }
+
+                {/*━━━━━(| Price |)━━━━━*/ }
+                <p>Price: ${ item.price }</p>
+
+                {/*━━━━━(| Total-amount |)━━━━━*/ }
+                <p>${ ( item.amount * item.price ).toFixed(2) }</p>
+
+                {/**━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━*/ }
+            </div>
+
+            {/*━━━━━(| Cart Buttons |)━━━━━*/ }
+            <div className="buttons">
+                {/**━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━*/ }
+
+                {/*━━━━━(| Button(Remove from cart)  |)━━━━━*/ }
+                <Button
+                    size="small"
+                    disableElevation
+                    variant="contained"
+                    onClick={ () => removeFromCart(item.id) }
+                > - </Button>
+
+                {/*━━━━━(| Amount |)━━━━━*/ }
+                <p>{ item.amount }</p>
+
+                {/*━━━━━(| Button(Remove from cart)  |)━━━━━*/ }
+                <Button
+                    size="small"
+                    disableElevation
+                    variant="contained"
+                    onClick={ () => addToCart(item) }
+                > + </Button>
+
+                {/**━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━*/ }
+            </div>
         </div>
+
+        {/*━━━━━(| Item Image |)━━━━━*/ }
+        <img src={ item.image } alt={ item.title } />
 
     </CartItemWrapper>
     // #™━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 )
 // END-OF: CartItem--
 export default CartItem
+
+
+
+
+
+
+
+
+
